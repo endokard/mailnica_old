0e334ddffe3b7e68d38a451cba288a1f61541eb8
endokard <45415540+endokard@users.noreply.github.com>
2019-07-08 23:43:41 +0300

    Create config.local.php

---
+++ postfixadmin/config.local.php
@@ -0,0 +1,9 @@
+#/var/www/admin/config.local.php
+<?php
+$CONF['database_type'] = 'mysqli';
+$CONF['database_user'] = 'postfix';
+$CONF['database_password'] = 'postfixadmin_pass';
+$CONF['database_name'] = 'postfix';
+
+$CONF['configured'] = true;
+?>
